a03269ccf71052b2f6757637b649e32f6fe7c0eb
Meredith Gallop <meredith@trtl.co.uk>
2021-01-14 14:07:58 +0000

    B-1 created add click function to utilise on button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect} from 'react';
+
 
 function App() {
+
+  const [clicks, setClicks] = useState(0);
+
+  const addClick = () => {
+    console.log("Button was clicked")
+  };
+
   return (
     <div className="App">
       <div>Welcome to the clicker app.</div>
       <div>
-        <button>
+  <div>You have clicked the button this many times: {clicks}</div>
+        <button
+        onClick={() => {
+          addClick()
+        }}
+        >
           Click Me
         </button>
       </div>
